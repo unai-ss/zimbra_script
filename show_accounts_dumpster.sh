4910ea2049c03417c4d47a9dcc692fa3e3de6a3c
Unai Solaguren <usolaguren@gmail.com>
2018-03-18 17:31:38 +0100

    Update show_accounts_dumpster.sh

--- show_accounts_dumpster.sh
+++ show_accounts_dumpster.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
 
+for acct in `/opt/zimbra/bin/zmprov -l gaa | grep -E -v '(^admin@|^spam\..*@|^ham\..*@|^virus-quarantine.*@|^galsync.*@)'|sort` ; do
+    echo "Show dumpster $acct"
+      zmmailbox -z -m $acct s --dumpster -l 30 --types message larger:1kb  
+    done 
+done
